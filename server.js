73f7abc51431dc43c55bc1070b516768bb5fbeb8
nguyenngochuy00 <ngochuy25120@gmail.com>
2022-08-09 13:57:52 +0700

    my first commit

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.use('/images', imageRoutes);
 //    }
 // })
 
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
+
 // const PORT = process.env.PORT || 5000
 
 server.listen(process.env.PORT || 5000, ()=> {

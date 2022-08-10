0770f1c746660e0eb28ba732a3d9e7420f34464e
nguyenngochuy00 <ngochuy25120@gmail.com>
2022-08-10 10:50:54 +0700

    frontend

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const appApi = createApi({
     reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://fd-mart.herokuapp.com/" }),
     endpoints: (builder) => ({
         signup: builder.mutation({  // changing things with db
             query: (user) => ({

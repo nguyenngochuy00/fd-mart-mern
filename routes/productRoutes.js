783ea340d80a863b9df618be758c91eeaf242036
nguyenngochuy00 <ngochuy25120@gmail.com>
2022-08-11 12:17:05 +0700

    Updated backend

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -75,7 +75,7 @@ router.get('/category/:category', async(req,res)=> {
   try {
     let products;
     const sort = {'_id': -1}
-    if(category == "list"){
+    if(category == "all"){
       products = await Product.find().sort(sort);
     } else {
       products = await Product.find({category}).sort(sort);

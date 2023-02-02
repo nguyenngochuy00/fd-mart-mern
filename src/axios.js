093924bbdfb1bfc9d9ee8f44491933e203ea6f4e
nguyenngochuy00 <ngochuy25120@gmail.com>
2023-02-02 12:03:21 +0700

    Update URL be

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL: "https://fd-mart.herokuapp.com/",
-    // baseURL: "https://be-fdmart-mern.onrender.com/",
-    baseURL: "http://localhost:5000",
+    baseURL: "https://be-fdmart-mern.onrender.com/",
+    // baseURL: "http://localhost:5000",
 
 });
 
